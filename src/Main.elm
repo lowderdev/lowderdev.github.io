1c75e0e5986bc62e9c46af2752bd500e93194960
Logan Lowder <logan.lowder@logikcull.com>
2021-11-19 17:11:07 -0600

    Initial commit, working app

--- src/Main.elm
+++ src/Main.elm
@@ -7,10 +7,12 @@ module Main exposing (..)
 --
 
 import Browser
-import Debug exposing (toString)
-import Html exposing (Html, button, div, text)
-import Html.Attributes exposing (style)
-import Html.Events exposing (onClick)
+import Css
+import Css.Global
+import Html.Styled exposing (..)
+import Html.Styled.Attributes as Attr
+import Tailwind.Breakpoints as Breakpoints
+import Tailwind.Utilities as Tw
 
 
 
@@ -19,7 +21,11 @@ import Html.Events exposing (onClick)
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view >> toUnstyled
+        }
 
 
 
@@ -58,45 +64,55 @@ update msg { grid } =
 -- VIEW
 
 
-view : Model -> Html Msg
+view : Model -> Html msg
 view { grid } =
-    div
-        [ style "margin-top" "2em"
-        , style "border" "red dashed 1px"
-        ]
-        [ viewGrid grid ]
-
-
-viewGrid : Grid -> Html Msg
-viewGrid grid =
-    div
-        [ style "display" "grid"
-        , style "justify-content" "center"
-        , style "grid-template-columns" "100px 50px 50px 100px"
-        , style "grid-template-rows" "auto"
-        , style "grid-template-areas"
-            ("grid-margin"
-                ++ ". row1 row1 ."
-                ++ ". row2 row2 ."
-            )
-        ]
-        (List.map
-            (\row ->
-                div []
-                    (List.indexedMap
-                        (\i rowValue ->
-                            div
-                                [ style "grid-area" ("row" ++ toString i)
-                                , style "height" "100px"
-                                , style "width" "100px"
-                                , style "margin" "1em auto"
-                                , style "border" "red dashed 1px"
-                                , style "text-align" "center"
-                                ]
-                                [ text rowValue ]
+    div [ Attr.css [ Tw.bg_gray_50, Tw.rounded_t_xl, Tw.p_24 ] ]
+        [ -- This will give us the standard tailwind style-reset as well as the fonts
+          Css.Global.global Tw.globalStyles
+        , div
+            [ Attr.css [ Tw.bg_blue_100, Tw.grid, Tw.grid_cols_2, Tw.w_24 ] ]
+            (List.map
+                (\row ->
+                    div []
+                        (List.map
+                            (\rowValue ->
+                                div
+                                    [ Attr.css [ Tw.bg_blue_500, Tw.rounded_sm, Tw.w_12, Tw.h_12, Tw.text_center ] ]
+                                    [ text rowValue ]
+                            )
+                            row
                         )
-                        row
-                    )
+                )
+                grid
             )
-            grid
-        )
+        , div []
+            [ a
+                [ Attr.css
+                    [ Tw.inline_flex
+                    , Tw.items_center
+                    , Tw.justify_center
+                    , Tw.px_5
+                    , Tw.py_3
+                    , Tw.border
+                    , Tw.border_transparent
+                    , Tw.text_base
+                    , Tw.font_medium
+                    , Tw.rounded_md
+                    , Tw.text_white
+                    , Tw.bg_indigo_600
+
+                    -- We can use hover styles via elm-css :)
+                    , Css.hover [ Tw.bg_indigo_700 ]
+                    ]
+                , Attr.href "#"
+                ]
+                [ text "Get started" ]
+            ]
+        ]
+
+
+
+-- viewBoard : Grid -> Html msg
+-- viewBoard grid =
+--   div [] [
+--   ]
